e802ed3fb8b711b240d892355273f3a8e401db9b
Anastasia Zhuravleva <97887955+meri-maki@users.noreply.github.com>
2023-02-24 22:27:29 +0300

    edited text, added skills

--- src/AllSections.jsx
+++ src/AllSections.jsx
@@ -9,77 +9,85 @@ import Courses from './components/Courses'
 import ContactForm from './components/ContactForm'
 
 const AllSections = ({ mode }) => {
-  return (
-    <>
-      <Section size="big" mode={mode} id="about">
-        <div className="section-about-text-block">
-          <h1>Hey✨ I’m Ana, Frontend Developer</h1>
-          <p>
-            I’m learning to code on Codecademy and YouTube. <br />
-            I’m passionate about coding, contemporary art and archery.
-          </p>
-          <div className="button-container">
-            <Link
-              hashSpy={true}
-              smooth={true}
-              offset={-50}
-              duration={500}
-              to="contact"
-            >
-              <Button color="pink">Contact me</Button>
-            </Link>
-            <a
-              href="Anastasia-Zhuravleva-CV-eng.pdf"
-              target="_blank"
-              download="Anastasia-Zhuravleva-CV-eng.pdf"
-            >
-              <Button color="green">Download CV</Button>
-            </a>
-          </div>
-        </div>
-        <MainPhotoCollage />
-      </Section>
-      <Section size="small" mode={mode} title="Skills & Tools">
-        <div>
-          <Skills />
-        </div>
-      </Section>
-      <Section mode={mode} title="Latest projects" id="projects">
-        <Projects mode={mode} />
-      </Section>
-      <Section mode={mode} title="Courses & Certificates" size="small">
-        <Courses mode={mode} />
-      </Section>
-      <Section mode={mode} title="Lets work together!" id="contact">
-        <div className="section-contacts">
-          <div className="section-contact-links">
-            <p>
-              Liked my work? I’m currently open for remote work as Junior or
-              Intern Frontend Developer or Web Developer.
-            </p>
-            <div className="button-container">
-              <a
-                href="Anastasia-Zhuravleva-CV-eng.pdf"
-                target="_blank"
-                download="Anastasia-Zhuravleva-CV-eng.pdf"
-              >
-                <Button color="green">CV ENG</Button>
-              </a>
-              <a
-                href="Anastasia-Zhuravleva-CV-RUS.pdf"
-                target="_blank"
-                download="Anastasia-Zhuravleva-CV-RUS.pdf"
-              >
-                <Button color="pink">CV RUS</Button>
-              </a>
-            </div>
-            <Contacts />
-          </div>
-          <ContactForm mode={mode} />
-        </div>
-      </Section>
-    </>
-  )
+    return (
+        <>
+            <Section size="big" mode={mode} id="about">
+                <div className="section-about-text-block">
+                    <h1>Hey✨ I’m Ana, Frontend React Developer</h1>
+                    <p>
+                        I've been working in the field of{' '}
+                        <span className="bold-p">design</span> and{' '}
+                        <span className="bold-p">web development</span> for over
+                        two years, and I've been entirely dedicated to the{' '}
+                        <span className="bold-p">frontend development</span>{' '}
+                        since June 2022.
+                        <br />
+                        <br />
+                        In my spare time, I enjoy hanging out with my family,
+                        watching movies and anime, and practicing archery.
+                    </p>
+                    <div className="button-container">
+                        <Link
+                            hashSpy={true}
+                            smooth={true}
+                            offset={-50}
+                            duration={500}
+                            to="contact"
+                        >
+                            <Button color="pink">Contact me</Button>
+                        </Link>
+                        <a
+                            href="Anastasia-Zhuravleva-CV-eng.pdf"
+                            target="_blank"
+                            download="Anastasia-Zhuravleva-CV-eng.pdf"
+                        >
+                            <Button color="green">Download CV</Button>
+                        </a>
+                    </div>
+                </div>
+                <MainPhotoCollage />
+            </Section>
+            <Section size="small" mode={mode} title="Skills & Tools">
+                <div>
+                    <Skills />
+                </div>
+            </Section>
+            <Section mode={mode} title="Latest projects" id="projects">
+                <Projects mode={mode} />
+            </Section>
+            <Section mode={mode} title="Courses & Certificates" size="small">
+                <Courses mode={mode} />
+            </Section>
+            <Section mode={mode} title="Lets work together!" id="contact">
+                <div className="section-contacts">
+                    <div className="section-contact-links">
+                        <p>
+                            Liked my work? I’m currently open for remote work
+                            <br /> as Junior+ / Middle Frontend Developer.
+                        </p>
+                        <div className="button-container">
+                            <a
+                                href="Anastasia-Zhuravleva-CV-eng.pdf"
+                                target="_blank"
+                                download="Anastasia-Zhuravleva-CV-eng.pdf"
+                            >
+                                <Button color="green">CV ENG</Button>
+                            </a>
+                            <a
+                                href="Anastasia-Zhuravleva-CV-RUS.pdf"
+                                target="_blank"
+                                download="Anastasia-Zhuravleva-CV-RUS.pdf"
+                            >
+                                <Button color="pink">CV RUS</Button>
+                            </a>
+                        </div>
+                        <Contacts />
+                    </div>
+                    <ContactForm mode={mode} />
+                </div>
+            </Section>
+        </>
+    )
 }
 
 export default AllSections
--- src/components/MainPhotoCollage.jsx
+++ src/components/MainPhotoCollage.jsx
@@ -1,20 +1,25 @@
-import MainPhoto from "../assets/main-photo.png"
-import GraphicGroup from "../assets/graphic-group.png"
-import Graphic1 from "../assets/graphic1.svg"
-import Graphic2 from "../assets/graphic2.svg"
-import Graphic3 from "../assets/graphic3.svg"
-import styles from "./MainPhotoCollage.module.css"
+import MainPhoto from '../assets/main-photo.png'
+import GraphicGroup from '../assets/graphic-group.png'
+import Graphic1 from '../assets/graphic1.svg'
+import Graphic2 from '../assets/graphic2.svg'
+import Graphic3 from '../assets/graphic3.svg'
+import styles from './MainPhotoCollage.module.css'
 
 const MainPhotoCollage = () => {
-  return (
-    <div className={styles.mainPhotoCollage}>
-      <img src={GraphicGroup} className={styles.graphicGroup} alt="photo collage"/>
-  {/*     <img src={MainPhoto} className={styles.mainPhoto} />
+    return (
+        <div className={styles.mainPhotoCollage}>
+            <img
+                src={GraphicGroup}
+                className={styles.graphicGroup}
+                alt="photo collage"
+                draggable={false}
+            />
+            {/*     <img src={MainPhoto} className={styles.mainPhoto} />
       <img src={Graphic1} className={styles.graphic1} />
       <img src={Graphic2} className={styles.graphic2} />
       <img src={Graphic3} className={styles.graphic3} /> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default MainPhotoCollage
